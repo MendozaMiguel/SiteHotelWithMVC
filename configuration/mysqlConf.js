af5cb6982820712786f7aa960f51f83f6b5bce0b
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-02-17 21:36:22 +0100

    bdd commenté

--- configuration/mysqlConf.js
+++ configuration/mysqlConf.js
@@ -4,12 +4,12 @@ const connSql = require('./configuration/mysqlConf');
 puis connSql.query("SELECT * ...
  */
 
-var mysql = require('promise-mysql');
-const connection = mysql.createConnection({
-    host     : '127.0.0.1',
-    user     : 'root',
-    password : "root",
-    database : 'hotel'
-})
+// var mysql = require('promise-mysql');
+// const connection = mysql.createConnection({
+//     host     : '127.0.0.1',
+//     user     : 'root',
+//     password : "root",
+//     database : 'hotel'
+// })
 
-module.exports = connection;
+// module.exports = connection;
